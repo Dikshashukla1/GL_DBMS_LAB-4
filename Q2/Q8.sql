f855a6f770e4fc870389f74792a618f3bcb45b3d
Dikshashukla1 <106049102+Dikshashukla1@users.noreply.github.com>
2023-02-28 23:19:06 +0530

    Update Q8.sql

--- Q2/Q8.sql
+++ Q2/Q8.sql
@@ -1,13 +1,6 @@
-# Que 8. 
-# Display customer name and gender whose names start or end with character 'A'.
+# Que 8. Display customer name and gender whose names start or end with character 'A'.
 SELECT * FROM customer AS c;
-
-# Exact Match
-SELECT * FROM customer AS c WHERE c.CUS_NAME = 'MEGHA';
-
 # Partial Match
-# Names starting with A or names ending with c.
+# Names starting with A or names ending with A.
  
 SELECT * FROM customer AS c WHERE (c.CUS_NAME LIKE  'A%' OR c.CUS_NAME LIKE  '%A');
-# Name starting with A and ending with N
-SELECT * FROM customer AS c WHERE c.CUS_NAME LIKE  'A%N';
